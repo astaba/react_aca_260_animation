58ef461f68131fc0e4d96f1d8d57c6275dbb4ca4
Astar <bahouidi99@gmail.com>
2023-09-02 17:54:39 +0100

    Animate color and staging Keyframes

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -65,7 +65,7 @@ export default function NewChallenge({ onDone }) {
             <motion.li
               variants={{
                 stateZero: { opacity: 0, scale: 0.5 },
-                visible: { opacity: 1, scale: 1 },
+                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
               }}
               exit={{ opacity: 1, scale: 1 }}
               transition={{ type: "spring" }}
