607194039d4d8f5d73a6357948bba43a3a485585
Astar <bahouidi99@gmail.com>
2023-09-03 12:28:42 +0100

    Animating shared elements

--- src/components/ChallengeTabs.jsx
+++ src/components/ChallengeTabs.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import Badge from "./Badge.jsx";
 
 function Tab({ isSelected, onSelect, badgeCaption, children }) {
@@ -10,7 +12,7 @@ function Tab({ isSelected, onSelect, badgeCaption, children }) {
         {children}
         <Badge caption={badgeCaption}></Badge>
       </button>
-      {isSelected && <div className="active-tab-indicator" />}
+      {isSelected && <motion.div layoutId="tab-indicator" className="active-tab-indicator" />}
     </li>
   );
 }
